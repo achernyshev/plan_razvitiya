7414e219c3706d42c661134e820fc8cd57b9f9d7
Alex <achernyshev@corp.web4pro.com.ua>
2015-11-03 10:17:42 +0200

    Test task

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once("CsvFileReader.php");
+require_once("XmlFileReader.php");
 
-require_once('Session.php');
+/*
+$xmlFile = new XmlFileReader();
+$xmlFile->open('test_files/xml_test.xml');
+echo '<pre>';
+echo 'File contents:</br>';
+var_dump($xmlFile->getContents());
+echo 'File Size: </br>';
+var_dump($xmlFile->getFileSize('test_files/xml_test.xml'));
+*/
 
-$session = new Session;
-// comment next line for testing
-//$_SESSION['test'] = false;
-var_dump($_SESSION['test']); die;
+/*
+$csvFile = new CsvFileReader();
+$csvFile->open('test_files/csv_test.csv');
+echo '<pre>';
+var_dump($csvFile->getContents());
+echo 'File Size: </br>';
+var_dump($csvFile->getFileSize('test_files/csv_test.csv'));
+*/
